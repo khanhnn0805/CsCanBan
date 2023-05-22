021213df87f1387de1135269847ec414be7cab9d
khanhnn0805 <nguyenngockhanh926@gmail.com>
2023-05-22 20:54:29 +0700

    Tim phan tu

---
+++ CsCanban/Buoi 05/Tinh tong cac phan tu/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tinh_tong_cac_phan_tu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //C1: Theo cach dung fo
+           
+            int[] a = new int[10];
+            Console.WriteLine("Vui long nhap phan tu");
+            int n = int.Parse(Console.ReadLine());//n la so luong pt
+
+            Console.WriteLine("Nha gia tri cac phan tu");
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Phan tu thu [" + i + "]=");
+                //luu gia tri vao mang
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine();
+            Console.WriteLine("Cac gia tri moi nhap");
+            for (int i = 0;i<n; i++)
+            { Console.WriteLine(a[i]); };
+
+            int tong = 0;
+            for (int i = 0; i < n; i++) tong += a[i];
+            {
+                Console.WriteLine("Tong gia tri trong mang =" + tong);
+            }
+            
+
+        }
+    }
+}
